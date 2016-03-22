a01c4bd9e4e072a0b5bdc7ca65be3927c16572bd
CRC User <CRC User@192.168.0.103>
2016-03-22 06:40:10 -0700

    Depreciating mcmod.info feature
    Establish independence from old mcmod.info model of data storage,
    through Constants.metadata

--- src/net/anglewyrm/pixies/Pixies.java
+++ src/net/anglewyrm/pixies/Pixies.java
@@ -1,12 +1,19 @@
 package net.anglewyrm.pixies;
 
-import org.apache.logging.log4j.Logger;
+import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Constants.modid, name = Constants.name, version = Constants.version)
-public class PixiesMain {
+public class Pixies {
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		Constants.getMetadata(event);
+	}
 	
     @EventHandler
     public void init(FMLInitializationEvent event){
