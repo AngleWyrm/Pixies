61227f3a7ef30c7bfe5f9e74db2440f72bdecf81
CRC User <CRC User@192.168.0.102>
2016-04-03 03:36:28 -0700

    Added recipe for day-1 sapling & stick bed

--- src/net/anglewyrm/pixies/Recipes.java
+++ src/net/anglewyrm/pixies/Recipes.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class Recipes {
 
@@ -19,15 +20,15 @@ public class Recipes {
 				new ItemStack(Items.redstone),
 				new ItemStack(Items.glowstone_dust) 
 				);
-		/*
-		GameRegistry.addRecipe(new ItemStack(Items.redstone), new Object[]
-				{
-			"G", 
-			'G', Items.iron_ingot
-				});		// redstone -> glowstone
-		*/
-		// sapling bed
 		
+		// early game sapling bed
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.bed), 
+				"   ",
+				"SSS",
+				"TTT",
+				'S', "treeSapling",
+				'T', Items.stick));
+				
 	} // registerRecipes
 
 } // Recipes class
